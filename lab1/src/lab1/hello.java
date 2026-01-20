e0de2c85b03ba3da5628b43599389f53f7cdceed
vudat169 <datvu2990@gmail.com>
2026-01-20 20:37:25 +0700

    vi du demo
    demo thu

---
+++ lab1/src/lab1/hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package lab1;
+
+/**
+ * 
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao.");
+	}
+
+}
